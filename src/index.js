960ed6da9205929a07498b3e49f299137e4aa7b6
thanvu1 <hoangthanvu11@gmail.com>
2024-05-22 22:06:43 +0700

    Your commit message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Sử dụng react-dom/client cho React 18 trở lên
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './style.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
